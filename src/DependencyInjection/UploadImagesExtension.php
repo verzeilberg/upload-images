a7cfa23883428cbe7be2fcca3af1c9e7dd64c31d
verzeilberg <sander@verzeilberg>
2020-08-13 14:32:22 +0200

    WIP

--- src/DependencyInjection/UploadImagesExtension.php
+++ src/DependencyInjection/UploadImagesExtension.php
@@ -4,7 +4,7 @@
 namespace verzeilberg\UploadImagesBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
@@ -13,8 +13,8 @@ class UploadImagesExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new XmlFileLoader($container, new FileLocator(dirname(__DIR__).'/Resources'));
-        $loader->load('services.xml');
+        $loader = new YamlFileLoader($container, new FileLocator(dirname(__DIR__).'/Resources'));
+        $loader->load('services.yaml');
 
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
