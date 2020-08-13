b44545135c2ae6e574a0d31ee21fd17fe8a2328b
verzeilberg <sander@verzeilberg>
2020-08-13 22:38:41 +0200

    WIP

--- src/DependencyInjection/UploadImagesExtension.php
+++ src/DependencyInjection/UploadImagesExtension.php
@@ -19,6 +19,6 @@ class UploadImagesExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $container->setParameter('upload-images.params', $config);
+        $container->setParameter('upload-images.config', $config);
     }
 }
