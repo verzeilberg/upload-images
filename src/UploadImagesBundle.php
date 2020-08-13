3785bbd44a69282c63f9c3fee9427187496d3fa5
verzeilberg <sander@verzeilberg>
2020-08-13 23:51:43 +0200

    WIP

--- src/UploadImagesBundle.php
+++ src/UploadImagesBundle.php
@@ -8,11 +8,5 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class UploadImagesBundle extends Bundle
 {
-    public function getContainerExtension()
-    {
-        if (null === $this->extension) {
-            $this->extension = new UploadImagesExtension();
-        }
-        return $this->extension;
-    }
+
 }
