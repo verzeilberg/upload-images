f9ea100f688385f15be98fc67d1e8565ef2ecb0b
verzeilberg <sander@verzeilberg>
2020-08-14 12:34:54 +0200

    WIP

--- src/DependencyInjection/UploadImagesExtension.php
+++ src/DependencyInjection/UploadImagesExtension.php
@@ -6,7 +6,7 @@ namespace verzeilberg\UploadImagesBundle\DependencyInjection;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
 class UploadImagesExtension extends Extension
