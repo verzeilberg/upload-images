e6ac9807844186b768bc65c61708ab272eefa03e
verzeilberg <sander@verzeilberg>
2020-09-03 23:40:59 +0200

    WIP

--- src/Form/Image/Upload.php
+++ src/Form/Image/Upload.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 use Verzeilberg\UploadImagesBundle\Entity\Image;
 
