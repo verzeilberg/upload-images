4a41da75f0342758ead851aba8ce8c574696d030
verzeilberg <sander@verzeilberg>
2020-09-14 23:16:48 +0200

    WIP

---
+++ src/EventListener/AnnotationListener.php
@@ -0,0 +1,72 @@
+<?php
+
+
+namespace verzeilberg\UploadImagesBundle\EventListener;
+
+use Doctrine\Common\EventArgs;
+
+/**
+ * UploadListener.
+ *
+ * Handles file uploads.
+ *
+ * @author
+ */
+class AnnotationListener extends BaseListener
+{
+    /**
+     * The events the listener is subscribed to.
+     *
+     * @return array The array of events
+     */
+    public function getSubscribedEvents(): array
+    {
+        return [
+            'prePersist',
+            'preUpdate',
+        ];
+    }
+
+    /**
+     * @param EventArgs $event The event
+     *
+     * @throws \Vich\UploaderBundle\Exception\MappingNotFoundException
+     */
+    public function prePersist(EventArgs $event): void
+    {
+
+        die('test');
+
+        $object = $this->adapter->getObjectFromArgs($event);
+
+        if (!$this->isUploadable($object)) {
+            return;
+        }
+
+        foreach ($this->getUploadableFields($object) as $field) {
+            $this->handler->upload($object, $field);
+        }
+    }
+
+    /**
+     * @param EventArgs $event The event
+     *
+     * @throws \Vich\UploaderBundle\Exception\MappingNotFoundException
+     */
+    public function preUpdate(EventArgs $event): void
+    {
+        die('test2');
+
+        $object = $this->adapter->getObjectFromArgs($event);
+
+        if (!$this->isUploadable($object)) {
+            return;
+        }
+
+        foreach ($this->getUploadableFields($object) as $field) {
+            $this->handler->upload($object, $field);
+        }
+
+        $this->adapter->recomputeChangeSet($event);
+    }
+}
