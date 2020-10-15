c20eee791b7834dcb8a4e4045b2f894f82c7624c
verzeilberg <sander@verzeilberg>
2020-10-16 00:15:22 +0200

    WIP

--- src/EventListener/ImageListener.php
+++ src/EventListener/ImageListener.php
@@ -5,10 +5,15 @@ namespace verzeilberg\UploadImagesBundle\EventListener;
 
 
 use verzeilberg\UploadImagesBundle\Entity\Image;
+use Doctrine\ORM\Event\LifecycleEventArgs;
+use Doctrine\ORM\Event\PreFlushEventArgs;
+use Doctrine\ORM\Event\PreUpdateEventArgs;
+use Doctrine\ORM\Mapping as ORM;
 
 class ImageListener
 {
 
+    /** @ORM\PrePersist */
     public function prePersistHandler(
         Image $image,
         LifecycleEventArgs $event
@@ -17,13 +22,13 @@ class ImageListener
         die('You shall not pass');
     }
 
-
+    /** @ORM\PostPersist */
     public function postPersistHandler(Image $image, LifecycleEventArgs $event)
     {
         die('You shall not pssssass');
     }
 
-
+    /** @ORM\PreUpdate */
     public function preUpdateHandler(Image $image, PreUpdateEventArgs $event)
     {
         die('You shall not sdadasdasdpass');
